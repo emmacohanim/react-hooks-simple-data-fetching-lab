5374bfdfcc9d7dd0b42e88299f464203f5e98f40
Emma Cohanim <emmacohanim@gmail.com>
2022-08-17 22:24:38 -0700

    commit tree 8c4dfc842a94d5c1a9408169737613e20f89b523

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App () {
+
+    // initialize state of data and set initial state to null
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        // pass fetch in as first argument to fetch pic from site on first render and update data state
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data.message);
+        })
+    }, []);
+
+    if (!data) return <p>Loading...</p>
+    // if data is false (null is false), render p
+    return (
+                <img src= {data} alt="A Random Dog" />
+                // render image by referencing updated state
+
+        )
+    }
+
+
+
+export default App
